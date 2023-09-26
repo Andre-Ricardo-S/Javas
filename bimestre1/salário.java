cb6fa7abddaea2fc445298a3afc077502e59e6fa
Andre-Ricardo-S <142915274+Andre-Ricardo-S@users.noreply.github.com>
2023-09-26 11:18:02 -0300

    Add files via upload

---
+++ bimestre1/salário.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class salário {
+	public static void main(String[] args) {
+		Scanner ler = new Scanner(System.in);
+		double Su, Qsm;
+		System.out.println("Coloque o seu salário");
+		Su = ler.nextDouble();
+		Qsm = Su/1320,00
+		System.out.println("A quantidade de salários mínimos que você ganha é"+Qsm);
+		
+	}
+
+}
