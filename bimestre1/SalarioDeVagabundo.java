80567893370fafc5f973ef220fb0775f095e2f0b
Andre-Ricardo-S <142915274+Andre-Ricardo-S@users.noreply.github.com>
2023-10-17 06:08:34 -0300

    Atualizar o SalarioDeVagabundo.java

--- bimestre1/SalarioDeVagabundo.java
+++ bimestre1/SalarioDeVagabundo.java
@@ -1,16 +1,16 @@
 import java.util.Scanner;
 
-public class SalarioDeVagabundo {
+public class Salario {
  public static void main (String [] args) {
 	 Scanner ler = new Scanner (System.in);
 	 double su ;
-	 System.out.println("informe aqui seu sal·rio");
+	 System.out.println("informe aqui seu sal√°rio");
 	 su = ler.nextDouble();
 	 if (su < 1320) {                                                                    
-		       System.out.println("teu sal·rio È uma merda, est· abaixo do sal·rio mÌnimo");
+		       System.out.println("teu sal√°rio √© uma merda, est√° abaixo do sal√°rio m√≠nimo");
 	 } 
 	 else {
-		 System.out.println("teu sal·rio t· bom, acima do sal·rio mÌnimo");
+		 System.out.println("teu sal√°rio t√° bom, acima do sal√°rio m√≠nimo");
 	 }
 	 
 	 
